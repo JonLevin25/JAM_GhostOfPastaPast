ad6a0da33ebb2f101265fe903e2d7dbdc7c03be0
Jonathan Levin <jonmlevin@outlook.com>
2020-01-16 23:47:42 +0200

    Catch WIP

---
+++ Assets/Catch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class Catch : MonoBehaviour
+{
+    [SerializeField] private Transform _catchPosition;
+    [SerializeField] private LayerMask _throwableLayerMask;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var isThrowable = ContainsLayer(_throwableLayerMask, other.gameObject.layer);
+        if (!isThrowable) return;
+        
+        var throwable = other.GetComponent<Throwable>();
+        if (throwable.IsHeldByPlayer) return;
+        
+        // TODO: WIP
+    }
+
+    private bool ContainsLayer(LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+}
