d41ec2834d3042ab16b910cec558ed54719cd7cc
Ohad Abarbanel <ohad@d-fendsolutions.com>
2020-01-17 06:01:40 +0200

    Added trap support

---
+++ Assets/Scripts/HorizontalTrap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalTrap : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+
+    [SerializeField]
+    Rigidbody2D body;
+
+    [SerializeField]
+    GameObject floor;
+
+    private bool movingLeft = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingLeft) {
+            body.velocity = new Vector2(-moveSpeed, 0);
+        } else {
+            body.velocity = new Vector2(moveSpeed, 0);
+        }
+        var xThreshold = floor.transform.localScale.x / 2;
+        if (transform.localPosition.x < -xThreshold) {
+            movingLeft = false;
+        } else if (transform.localPosition.x > xThreshold) {
+            movingLeft = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHitbox.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitbox : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask trapLayerMask;
+
+    [SerializeField]
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var isTrap = ContainsLayer(trapLayerMask, other.gameObject.layer);
+        if (!isTrap) return;
+        
+        // player.TakeDamage();
+    }
+
+
+    private bool ContainsLayer(LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+}
