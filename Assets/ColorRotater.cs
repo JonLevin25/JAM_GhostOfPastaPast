5677f5cf3064f510a34a541e7235d9f37cf14a14
Jonathan Levin <jon.levin25@gmail.com>
2020-01-17 18:39:19 +0200

    Scenery

---
+++ Assets/ColorRotater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRotater : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _rend;
+
+    [SerializeField] private float _degreesPerSec;
+    [SerializeField] private bool _reverse;
+
+    private void Update()
+    {
+        var delta = (_degreesPerSec/360) * Time.deltaTime;
+        if (_reverse) delta *= -1;
+        
+        Color.RGBToHSV(_rend.color, out var h, out var s, out var v);
+        
+        h += delta;
+        h %= 360;
+        var newColor = Color.HSVToRGB(h, s, v);
+
+        _rend.color = newColor;
+    }
+}
