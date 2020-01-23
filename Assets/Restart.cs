a9ce094f99b8d04d3050cd45dc79e32595782280
Jonathan Levin <jonmlevin@outlook.com>
2020-01-23 22:10:29 +0200

    player hp + restart

---
+++ Assets/Restart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+public void RestartNOW(){
+	SceneManager.LoadScene(0);
+}
+ public void Update(){
+ if (Input.GetKeyDown(KeyCode.Alpha1)){
+RestartNOW();	
+}
+}
+}
